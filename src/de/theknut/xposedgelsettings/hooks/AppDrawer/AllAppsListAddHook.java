c77aeb582860d098e13887373e4591752431f196
Dary <anyofyou@gmail.com>
2014-03-06 18:08:53 +0800

    add feature to change app label

--- src/de/theknut/xposedgelsettings/hooks/AppDrawer/AllAppsListAddHook.java
+++ src/de/theknut/xposedgelsettings/hooks/AppDrawer/AllAppsListAddHook.java
@@ -5,6 +5,8 @@ import static de.robv.android.xposed.XposedHelpers.getObjectField;
 import android.content.ComponentName;
 
 import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedHelpers;
+import de.theknut.xposedgelsettings.AllAppsListToRename;
 import de.theknut.xposedgelsettings.hooks.PreferencesHelper;
 
 public final class AllAppsListAddHook extends XC_MethodHook {
@@ -21,5 +23,11 @@ public final class AllAppsListAddHook extends XC_MethodHook {
 			// don't add it to the allAppsList if it is in our list
 			param.setResult(null);
 		}
+        
+        for (String item : PreferencesHelper.renamedApps){
+            if (item.split(AllAppsListToRename.separator)[0].equals(componentName.getPackageName())){
+                XposedHelpers.setObjectField(param.args[0], "title", item.split(AllAppsListToRename.separator, 2)[1]);
+            }
+        }
 	}
 }
