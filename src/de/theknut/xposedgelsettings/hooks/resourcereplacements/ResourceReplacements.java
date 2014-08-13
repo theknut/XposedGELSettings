ba68d931e071ff473358ba728b4b7f92cf59edde
theknut <alexander.as.schulz@gmail.com>
2014-08-13 22:44:08 +0200

    Prevent crash when resource isn't found

--- src/de/theknut/xposedgelsettings/hooks/resourcereplacements/ResourceReplacements.java
+++ src/de/theknut/xposedgelsettings/hooks/resourcereplacements/ResourceReplacements.java
@@ -1,15 +1,22 @@
 package de.theknut.xposedgelsettings.hooks.resourcereplacements;
 
+import android.content.res.Resources;
+
 import de.robv.android.xposed.callbacks.XC_InitPackageResources.InitPackageResourcesParam;
+import de.theknut.xposedgelsettings.hooks.HooksBaseClass;
 import de.theknut.xposedgelsettings.hooks.PreferencesHelper;
 
 /**
  * Created by Alexander Schulz on 12.08.2014.
  */
-public class ResourceReplacements {
+public class ResourceReplacements extends HooksBaseClass{
 
     public static void initAllReplacements(InitPackageResourcesParam resparam) {
 
-        resparam.res.setReplacement(resparam.packageName, "color", "outline_color", PreferencesHelper.glowColor);
+        try {
+            resparam.res.setReplacement(resparam.packageName, "color", "outline_color", PreferencesHelper.glowColor);
+        } catch (Resources.NotFoundException nte) {
+            // not working on Trebuchet for no reason...
+        }
     }
 }
