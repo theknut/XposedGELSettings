459c6d3933075b41565da921719a23905024f7a5
theknut <alexander.as.schulz@gmail.com>
2014-08-14 17:38:59 +0200

    Moved Resource replacements to seperate class
    If resource replacements are disabled Xposed will only skip this class
    instead of the whole module.

---
+++ src/de/theknut/xposedgelsettings/hooks/ResourceReplacements.java
@@ -0,0 +1,32 @@
+package de.theknut.xposedgelsettings.hooks;
+
+import android.content.res.Resources;
+
+import de.robv.android.xposed.IXposedHookInitPackageResources;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.callbacks.XC_InitPackageResources.InitPackageResourcesParam;
+
+/**
+ * Created by Alexander Schulz on 12.08.2014.
+ */
+public class ResourceReplacements extends XC_MethodHook implements IXposedHookInitPackageResources {
+
+    @Override
+    public void handleInitPackageResources(InitPackageResourcesParam resparam) throws Throwable {
+        if (!Common.PACKAGE_NAMES.contains(resparam.packageName)) {
+            return;
+        }
+
+        PreferencesHelper.init();
+        ResourceReplacements.initAllReplacements(resparam);
+    }
+
+    public static void initAllReplacements(InitPackageResourcesParam resparam) {
+
+        try {
+            resparam.res.setReplacement(resparam.packageName, "color", "outline_color", PreferencesHelper.glowColor);
+        } catch (Resources.NotFoundException nte) {
+            // not working on Trebuchet for no reason...
+        }
+    }
+}
